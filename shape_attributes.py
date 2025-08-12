f693d241831f053b1fffc76e4df31ece93381189
Cheng Ding <chengding2021@gmail.com>
2025-08-12 09:58:51 -0500

    adding a few functions

--- shape_attributes.py
+++ shape_attributes.py
@@ -8,10 +8,13 @@ import math
 ## Define your functions to calculate the attributes below:
 
 ### Area
-
+def Area(num):
+    return math.pi * r * r
+def Sphere(num):
+    return 4 * math.pi * r * r
 #### print your results here:
-#print(f"Area of circle with radius size {r} = {<name_of_function>(r):.2f}")
-#print(f"Area of sphere with radius size {r} = {<name_of_function>(r):.2f}")
+print(f"Area of circle with radius size {r} = {Area(r):.2f}")
+print(f"Area of sphere with radius size {r} = {Sphere(r):.2f}")
 
 ### Diameter
 
